8b22047fdc4abd25bff94ab44f6ddb66ef792a0b
Marco Tavares <matr192001@hotmail.com>
2023-06-13 18:59:45 -0600

    Cambio en el inicio

---
+++ public/js/carousel.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveWidth: true
+  });
+});
